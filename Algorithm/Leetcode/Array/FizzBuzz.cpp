55ec4903b757536e2a7da23f75b45b095c3a19a9
SummerZm <1254551981@qq.com>
2020-04-23 20:19:18 +0800

    1. Leetcode - Rotate/Fizzbuzz.

---
+++ Algorithm/Leetcode/Array/FizzBuzz.cpp
@@ -0,0 +1,51 @@
+/*
+ * 问题：写一个程序，输出从 1 到 n 数字的字符串表示。
+ *      1. 如果 n 是3的倍数，输出“Fizz”；
+ *      2. 如果 n 是5的倍数，输出“Buzz”；
+ *      3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”
+ *
+ * 完成度: 80% - 2
+ * 前置条件: NULL.
+ * 注解: NULL
+ */
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>  containor;
+        for (int i=1; i<=n; i++) {
+            if  (i%3==0 && i%5==0)  {
+                containor.push_back("FizzBuzz");
+            }
+            else if (i%3==0) {
+                containor.push_back("Fizz");
+            }
+            else if (i%5==0) {
+                containor.push_back("Buzz");
+            }
+            else {
+                containor.push_back(to_string(i));
+            }
+        }
+        return containor;
+    }
+
+    vector<string> fizzBuzz2(int n) {
+        vector<string> containtor;
+        map<int, string> dict;
+        dict[3] = "Fizz";
+        dict[5] = "Buzz";
+        for (int i=1; i<=n; i++) {
+            string ret;
+            for (auto f:dict) {
+                if (i%f.first==0) {
+                        ret += f.second;
+                }
+            }
+            if (ret.empty()) {
+                ret = to_string(i);
+            }
+            containtor.push_back(ret);
+        }
+        return  containtor;
+    }
+}; 
---
+++ Algorithm/Leetcode/Array/Rotate.cpp
@@ -0,0 +1,41 @@
+/*
+ * 问题：给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * 完成度: 30%
+ * 前置条件: 空间复杂度为 O(1) 的 原地 算法
+ * 法1：暴力每次移动1个位置，移动k次.
+ * 法2：额外数组 - 空间复杂度 O(n).
+ * 法3：把数组当成一个环 - 会形成闭环.
+ * 法4：将数组进行两次反转.
+ * 注解: NULL
+ */
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int tmp = 0;
+        int len = nums.size();
+
+        // For input: [[1,2,3]  4
+        if (k>len) k -= len;
+
+        // Reverse  ALL
+        for (int i=0; i<len/2; i++) {
+            tmp = nums[len-i-1];
+            nums[len-i-1] = nums[i];
+            nums[i] = tmp;
+        }
+
+        // Reverse  [0, k)
+        for (int i=0; i<k/2; i++) {
+            tmp = nums[k-i-1];
+            nums[k-i-1] = nums[i];
+            nums[i] = tmp;
+        }
+        
+        // Reverse  [k, len)
+        for (int i=k; i<(len+k)/2; i++) {
+            tmp = nums[len-i-1+k];
+            nums[len-i-1+k] = nums[i];
+            nums[i] = tmp;
+        }
+    }
+};
